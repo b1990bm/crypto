31d4f50ccc4bc305a38e16ecb8753f4fb92b348b
Behnam <b1990bm@gmail.com>
2022-04-05 22:25:25 +0430

    create Filter

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,18 +3,54 @@ import React,{useState,useEffect} from 'react';
 //Api
 import {getCoin} from "../service/Api";
 
+//Loader
+import Loader from "./Loader";
+
+//Coin
+import Coin from "./Coin";
+
 const Landing = () => {
+
+    const [coins,setCoin]=useState([])
+    const [search,setSearch]=useState([])
 
     //fetchApi
     useEffect(()=>{
         const fetchApi=async()=>{
             const data=await getCoin();
-            console.log(data);
+            setCoin(data)
         }
         fetchApi();
     },[] )
+    const searchHandeler=(e)=>{
+        setSearch(e.target.value)
+    }
+    const searchCoins=coins.filter(coin=>coin.name.toLowerCase().includes(search))
     return (
         <div>
+            
+            <input type="text" placeholder='Search' value={search} onChange={searchHandeler}/>
+            {
+                coins.length ?
+                <div>
+                        {
+                        searchCoins.map(item=><Coin 
+                            key={item.id} 
+                            name={item.name}
+                            image={item.image}
+                            symbol={item.symbol}
+                            price={item.price}
+                            marketCap={item.marketCap}
+                            priceChange={item.market_cap_change_24h}
+
+                            />)
+                        }
+                </div> :
+
+                <Loader />
+
+            }
+            
             
         </div>
     );
